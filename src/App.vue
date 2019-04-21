091c3d60c6aa67c26d4270b589acc6b19898a68c
Frank-Huang-bo <44944715+Frank-Huang-bo@users.noreply.github.com>
2019-04-21 21:27:52 +0800

    Create .babelrc

---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-light bg-light">
+      <router-link class="navbar-brand" to="/">
+        <i class="fa fa-heart text-info" aria-hidden="true"></i>
+        FinalFantasy Game Park
+      </router-link>
+      <!-- 購物車內的數量 (Button 內包含 icon, 數量 badge) -->
+      <div class="dropdown ml-auto">
+        <button class="btn btn-sm btn-cart" data-toggle="dropdown" data-flip="false">
+          <i class="fa fa-shopping-cart text-dark fa-2x" aria-hidden="true"></i>
+          <span class="badge badge-pill badge-danger">{{cart.carts.length}}</span>
+          <span class="sr-only">unread messages</span>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right p-3" style="min-width: 300px"
+          data-offset="400">
+          <h6>已選擇商品</h6>
+          <table class="table table-sm">
+            <tbody>
+              <tr v-for="item in cart.carts" :key="item.id" v-if="cart.carts.length">
+                <td class="align-middle text-center">
+                  <a href="#" class="text-muted" @click.prevent="removeCart(item.id)">
+                    <i class="fa fa-trash-o" aria-hidden="true"></i>
+                  </a>
+                </td>
+                <td class="align-middle">{{ item.product.title }}</td>
+                <td class="align-middle">{{ item.qty }}{{item.product.unit}}</td>
+                <td class="align-middle text-right">{{item.total}}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button class="btn btn-primary btn-block">
+            <i class="fa fa-cart-plus" aria-hidden="true"></i> 結帳去
+          </button>
+        </div>
+      </div>
+    </nav>
+    <div class="jumbotron jumbotron-fluid jumbotron-bg d-flex align-items-end">
+      <div class="container">
+        <div class="p-3 bg-lighter">
+          <h1 class="display-3 font-weight-bold">太空戰士XV 最後出清</h1>
+          <p class="lead">Final Fantasy final stage, the end of Final Fantasy.</p>
+        </div>
+      </div>
+    </div>
+    <Loading :active.sync="isLoading"></Loading>
+    <router-view/>
+    <footer class="bg-light text-muted py-5">
+      <div class="container">
+        <ul class="list-inline text-center">
+          <li class="list-inline-item">© Copright 2019 太空電商</li>
+          <li class="list-inline-item">
+            <a class="text-info" href="#">
+              <i class="fa fa-instagram" aria-hidden="true"></i> Instagrame</a>
+          </li>
+          <li class="list-inline-item">
+            <a class="text-info" href="#">
+              <i class="fa fa-facebook-square" aria-hidden="true"></i> Facebook</a>
+          </li>
+          <li class="list-inline-item">
+            <a class="text-info" href="#">About</a>
+          </li>
+        </ul>
+        <p class="text-center">以上圖片僅供學習使用，無商業用途</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  name: 'App',
+  data() {
+    return {
+    };
+  },
+  methods: {
+    removeCart(id) {
+      this.$store.dispatch('cartsModules/removeCart', id);
+    },
+    ...mapActions('cartsModules', ['getCart']),
+  },
+  computed: {
+    ...mapGetters('cartsModules', ['cart']),
+    ...mapGetters(['isLoading']),
+  },
+  created() {
+    this.getCart();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+.jumbotron-bg {
+  /* banners.png */
+  background: url('https://static.gamespot.com/uploads/original/1551/15511094/3082826-3000664-final-fantasy-xv.jpg');
+  background-size: cover;
+  background-position: center center;
+  min-height: 400px;
+}
+/* 半透明背景 */
+.bg-lighter {
+  background-color: rgba(255, 255, 255, .45);
+}
+/* 購物車按鈕 */
+.btn-cart {
+  background-color: transparent;
+  position: relative;
+}
+/* 購物車按鈕定位 */
+.btn-cart .badge {
+  position: absolute;
+  top: 1px;
+  right: 1px;
+}
+.main-content {
+  min-height: calc(100vh - 56px - 176px)
+}
+.box-shadow {
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, .05);
+  transition: .3s linear;
+}
+.box-shadow:hover {
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, .08);
+}
+.dropdown-menu-right {
+  right: 0;
+  left: auto;
+}
+.alert-rounded {
+  border-radius: 50px;
+}
+</style>
